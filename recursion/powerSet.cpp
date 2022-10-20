61cda5138a207d5acc6f2e7fc2c2d946cb70e7b5
Rajat Singh <rsingh1734@gmail.com>
2022-10-20 22:57:10 +0530

    oct 20 night

---
+++ recursion/powerSet.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans){
+    //base
+    if(index >= nums.size()){
+        ans.push_back(output);
+        return ;
+    }
+    //exclude
+    solve(nums, output, index+1, ans);
+
+    //include
+    int element = nums[index];
+    output.push_back(element);
+    solve(nums, output, index+1, ans);
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ans;
+    vector<int> output;
+    int index = 0;
+    solve(nums, output, index, ans);
+    return ans;
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4,5};
+    vector<vector<int>> ans = subsets(nums);
+    cout<<endl<<"{ ";
+    for(int i=0;i<ans.size();i++){
+        cout<<"{ ";
+        for(int j=0;j<ans[i].size();j++)
+            cout<<" "<<ans[i][j];
+        cout<<" }";
+    }
+    cout<<" }"<<endl;
+    return 0;
+}
